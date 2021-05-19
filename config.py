1c57466668f490e343b8eabc30157ae106b3f6ae
Berlin <70908119+Berl4n@users.noreply.github.com>
2021-05-19 22:09:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 token = "1447847227:AAFth_610FvdVEmoj4PGKqfJE-6ekm9WCno"
 id = "768779619"
-QIWI_TOKEN = "text"
-QIWI_ACCOUNT = "text"
-channel = "@o875463thvg4n786b"
+QIWI_TOKEN = " "
+QIWI_ACCOUNT = " "
 op = "@por0vos1k"
